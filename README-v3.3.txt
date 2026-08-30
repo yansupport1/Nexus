25fa7fd48d80df0fd6bab3d36b8684874781f0e4
yansupport1 <yansupport1@gmail.com>
2026-08-30 19:58:18 +0700

    Last Sync: 2026-08-30 19:58 (Mobile)

---
+++ README-v3.3.txt
@@ -0,0 +1,1 @@
+Nexus-X v3.3\nSource index.html dengan fitur lama dipertahankan dan modul v3.3 tambahan.\nAuto-refresh wajib 3 detik tanpa tombol ON/OFF.\nFile .sh arbitrary dan bypass perangkat tidak dijalankan.\n
---
+++ modules/v33-feature-module.js
@@ -0,0 +1,7 @@
+/* Nexus-X v3.3 shared module API. The integrated index.html is the runtime entry point. */
+window.NexusModules=window.NexusModules||{};
+NexusModules.sensitivity=NexusModules.sensitivity||{};
+NexusModules.gacha=NexusModules.gacha||{};
+NexusModules.iq=NexusModules.iq||{};
+NexusModules.request=NexusModules.request||{};
+NexusModules.gameCheck=NexusModules.gameCheck||{};
